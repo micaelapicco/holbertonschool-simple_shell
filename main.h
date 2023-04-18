13baa872dd8775d10c2dc4fb87e3318f3311f682
micaelapicco <6314@holbertonstudents.com>
2023-04-18 07:14:16 -0700

    fix prompt

--- main.h
+++ main.h
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <string.h>
 int main(int ac, char **av);
 
 #endif
--- prompt.c
+++ prompt.c
@@ -11,23 +11,23 @@ int main(int ac, char **av)
 	(void)ac;
 
 	size_t n_bytes = 0;
-	ssize_t error;
-	char *prompt = "$ ", *buffer;
+	ssize_t n_chars_read;
+	char *prompt = "$ ", *buffer = NULL;
 
 	while (1)
-	{	
+	{
 	buffer = malloc(sizeof(1024));
 	if (!buffer)
 		return (-1);
 
 	printf("%s", prompt);
-	error = getline(&buffer, &n_bytes, stdin);
-		if (error == -1)
+	n_chars_read = getline(&buffer, &n_bytes, stdin);
+		if (n_chars_read == -1)
 		{
 			printf("end-of-file\n");
 			return (-1);
 		}
-	printf("%s\n", buffer);
+	printf("%s", buffer);
 	free(buffer);
 	}
 	return (0);
