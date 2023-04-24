ed1e1f23a747ec75be106c112b799a903662b0b1
vicenzoanza <viceanza2010@hotmail.com>
2023-04-24 12:37:45 -0700

    betty

--- count.c
+++ count.c
@@ -6,16 +6,16 @@
 */
 int count_str(char *str)
 {
-    int count = 0;
-    char *copy, *token;
+	int count = 0;
+	char *copy, *token;
 
-    copy = strdup(str);
-    token = strtok(copy, DELIM);
-    for (count = 0; token; count++)
-    {
-        token = strtok(NULL, DELIM);
-    }
-    free(copy);
-    free(token);
-    return (count);
+	copy = strdup(str);
+	token = strtok(copy, DELIM);
+	for (count = 0; token; count++)
+	{
+	token = strtok(NULL, DELIM);
+	}
+	free(copy);
+	free(token);
+	return (count);
 }
